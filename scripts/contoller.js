c42f42684b5a8fdf3b4da6a1f69fc8f6a7f4d730
khushijain3103 <khushijain312003@gmail.com>
2022-01-25 23:23:27 +0530

    finalised all the functioning

--- scripts/contoller.js
+++ scripts/contoller.js
@@ -2,18 +2,42 @@ window.addEventListener('load',eventbinder());
 
 function eventbinder(){
     var buttons = document.querySelectorAll('Button');
-    console.log(buttons);
+
     var input = document.querySelector("#input");
 
-    for(var idx=0 ; idx<buttons.length ; idx++)
+    for(var idx=0 ; idx<buttons.length-2 ; idx++)
     {
         buttons[idx].addEventListener('click' , function(event){
-            // console.log(event);
-            // console.log(buttons[idx].innerText);
             const val = event.target.innerText;
             input.value+=val;
         });
     }
+
+    var clear = document.querySelector('#clear');
+    clear.addEventListener('click',function(){
+        let insertedVal = '';
+        insertedVal+=input.value;
+        const newVal= insertedVal.slice(0,-1);
+
+        input.value='';
+        input.value+=newVal;
+    });
+
+    var enter = document.querySelector('#enter') ;
+    
+
+   enter.addEventListener('click',function(){
+        var span = document.querySelector('span');
+        var ans  ;
+        try{
+            ans= eval(input.value);
+        }
+        catch{
+            ans= "Wrong Expression";
+        }
+        
+        span.innerText = ans;
+    });
 }
 
 
